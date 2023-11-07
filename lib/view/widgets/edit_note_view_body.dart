6a78807ad442c3c9e838e09891a44c5f11c23811
omar <codeomar3@gmail.com>
2023-11-07 22:44:29 +0200

    add fields to edit note view

--- lib/view/widgets/edit_note_view_body.dart
+++ lib/view/widgets/edit_note_view_body.dart
@@ -1,21 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:noteapp/view/widgets/costom_app_bar.dart';
+import 'package:noteapp/view/widgets/costom_text_field.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({super.key});
-
   @override
   Widget build(BuildContext context) {
+    final TextEditingController titelControler = TextEditingController();
+    final TextEditingController contentControler = TextEditingController();
+
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        children: [
-          CostomAppBar(
-            iconData: Icons.done,
-            titel: 'Edit',
-            onPressed: () {},
-          ),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            CostomAppBar(
+              iconData: Icons.done,
+              titel: 'Edit',
+              onPressed: () {},
+            ),
+            SizedBox(height: 32),
+            CostomTextField(
+              controller: titelControler,
+              hintText: 'Titel',
+            ),
+            SizedBox(height: 16),
+            CostomTextField(
+              controller: contentControler,
+              hintText: 'Content',
+              maxLines: 5,
+            ),
+          ],
+        ),
       ),
     );
   }
